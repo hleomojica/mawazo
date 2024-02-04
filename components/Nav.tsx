9b90219c3a0112191aa5474c763f71d99f4d09d4
Henrry Mojica <hleomojica@gmail.com>
2024-02-04 20:22:26 +1100

    feat: add loggin options

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,6 +8,16 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
   const isUserLoggedIn = true;
 
+  const [provider, setProvider] = useState<any>(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProvider(response);
+    };
+    setProviders();
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
