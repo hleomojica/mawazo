615da25c13af60683d1eb87711adb175c1980925
Henrry Mojica <hleomojica@gmail.com>
2024-02-05 21:42:59 +1100

    feat: providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,13 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
