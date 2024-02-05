ade0d8b915b2ab8b9602349267e0afbeabf1ac0a
Henrry Mojica <hleomojica@gmail.com>
2024-02-05 21:26:44 +1100

    feat: nav bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,6 +9,7 @@ const Nav = () => {
   const isUserLoggedIn = true;
 
   const [providers, setProvider] = useState<any>(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
@@ -53,6 +54,7 @@ const Nav = () => {
                 width={40}
                 height={40}
                 className="rounded-full"
+                onClick={() => setToggleDropdown((prev) => !prev)}
               ></Image>
             </Link>
           </div>
@@ -83,11 +85,51 @@ const Nav = () => {
               width={37}
               height={37}
               className="rounded-full"
-              onClick={() => {}}
-            ></Image>
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt{" "}
+                </Link>
+                <button
+                  type="button"
+                  className="mt-5 w-full black_btn"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider: any) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
